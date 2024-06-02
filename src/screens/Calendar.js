825a63b1b96207c0fcbd8c18a339c613db26f10c
PolishRocketForce <122593167+PolishRocketForce@users.noreply.github.com>
2024-06-02 21:07:37 +0800

    CalendarPage.js

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
-export default function Calendar() {
-    return (
-        <View>
-            <Text>Calendar</Text>
-        </View>
-    );
-}
+const CalendarPage = () => {
+  const [selectedDate, setSelectedDate] = useState('');
+
+  const onDayPress = (day) => {
+    setSelectedDate(day.dateString);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Calendar
+        onDayPress={onDayPress}
+        markedDates={{
+          [selectedDate]: { selected: true, marked: true, selectedColor: 'blue' }
+        }}
+      />
+      <Text style={styles.selectedDateText}>Selected Date: {selectedDate}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  selectedDateText: {
+    marginTop: 20,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
+
+export default CalendarPage;
